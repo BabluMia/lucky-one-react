f30750f87b3eb80735bf3231172aafc3eb8e4c2e
Bablu Mia <bablumiah2000@gmail.com>
2022-03-25 21:18:05 +0600

    header and data created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,23 @@
 import './Header.css'
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <h2>hi</h2>
+            <>
+        <Navbar className="ps-5" bg="dark" variant="dark">
+          <Container className="w-75 mx-auto">
+            <Navbar.Brand href="#home">GRAB YOU'R ITEM</Navbar.Brand>
+            <Nav className="me-5 pe-5">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#item">Item</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
         </div>
     );
 };
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,12 +1,25 @@
-import './Products.css'
-import React from 'react';
+import "./Products.css";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-    return (
-        <div>
-            <h2>hi 2</h2>
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div>
+      <div className="row gx-5 container">
+        <div className="col-lg-10">
+          {products.map((product) => console.log(product.img))}
         </div>
-    );
+        <div className="col-lg-2 ">
+          <h2>hi</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
