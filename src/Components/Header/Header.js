35adf579c48b0a98aacdf9f02c8521a8f89581d0
Bablu Mia <bablumiah2000@gmail.com>
2022-03-25 20:35:43 +0600

    folder done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>hi</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Components/Products/Products.js
@@ -0,0 +1,12 @@
+import './Products.css'
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>hi 2</h2>
+        </div>
+    );
+};
+
+export default Products;
