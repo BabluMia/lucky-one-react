28210e05b2be9f817ce8084f6aff8055d106b8e4
Bablu Mia <bablumiah2000@gmail.com>
2022-03-26 16:29:39 +0600

    warning clear

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Name Of Project : Buy Now || React
 
 ## I use Bootstrap And React In This Project
-Live Link
-[Go to live link](https://buy-now-react-by-bablu.netlify.app/).
 
-## Available Scripts
+## Available On live
+[Click Here To See Demo](https://buy-now-react-by-bablu.netlify.app/).
+
+
 
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -5,13 +5,15 @@ import { BsCheckCircleFill } from "react-icons/bs";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+   const [addName, setAddName] = useState([]);
+   const [randomValue , setRandomValue] = useState('')
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
 
-  const [addName, setAddName] = useState([]);
+ 
   const click = (product) => {
     if (addName.length < 4){
       if(!addName.includes(product.name)){
@@ -23,28 +25,36 @@ const Products = () => {
   // for clear array
   const clearArray = () =>{
     setAddName([])
+    setRandomValue([])
   }
   // for random value
-  const [randomValue , setRandomValue] = useState('')
   
 const random = ()=> {
-    setRandomValue(Math.floor(Math.random()*addName.length)) 
+    // setRandomValue(Math.floor(Math.random()*addName.length)) 
+    const choice = addName[Math.floor(Math.random()*addName.length)]
+    setRandomValue(choice)
 }
 
   return (
     <div className="main">
       <div className="row  container my-5 mx-auto  py-5 ">
       <div data-aos="fade-right" className="col-lg-3 mb-5">
-          <h4 className="text-center me-5">Your Item</h4>
+          <h4 className="text-center me-5">Selected Item</h4>
           <ul>
             {addName.map((add) => (
-              <li className={addName[randomValue] === add && "text-primary"} key={add}>
+              // <li className={addName[randomValue] === add && "text-primary"} key={add}>
+              //   <BsCheckCircleFill></BsCheckCircleFill> <span className="ms-2">{add}</span>
+              // </li>
+              <li key={add}>
                 <BsCheckCircleFill></BsCheckCircleFill> <span className="ms-2">{add}</span>
               </li>
             ))}
           </ul>
+          
           <button className="custom-btn ms-2" onClick={random}>Choose One </button>
           <button className="custom-btn ms-2" onClick={()=>{clearArray()}}> Clear Cart </button>
+          <h3 className="ms-2 my-4">Choosed Item :</h3>
+          <h5 className="mt-2 ms-2 "> {randomValue}</h5>
         </div>
         <div className="col-lg-9 main row ">
           {products.map((product) => (
